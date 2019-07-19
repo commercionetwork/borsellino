9710a2dc5d3659119deabd6c8a087e6562f596d3
Bragaz <Merda10#>
2019-07-20 00:53:52 +0200

    principal send page and navbar

---
+++ lib/pages/send_coins/components/send_coins_body.dart
@@ -0,0 +1,10 @@
+import 'package:borsellino/pages/send_coins/components/send_coins_form.dart';
+import 'package:flutter/material.dart';
+
+/// Body that is displayed inside the send coins page
+class SendCoinsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SendCoinsForm();
+  }
+}
---
+++ lib/pages/send_coins/send_coins_page.dart
@@ -0,0 +1,15 @@
+import 'package:borsellino/pages/send_coins/components/send_coins_body.dart';
+import 'package:flutter/material.dart';
+
+
+class SendCoinsPage extends StatefulWidget {
+  @override
+  _SendCoinsPageState createState() => _SendCoinsPageState();
+}
+
+class _SendCoinsPageState extends State<SendCoinsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SendCoinsBody();
+  }
+}
