eed2fb05ce44d8a1993f73a286d0a53476b8ca07
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-07-20 13:29:19 +0200

    Improved the send page UI

--- lib/pages/send_coins/components/form_item.dart
+++ lib/pages/send_coins/components/form_item.dart
@@ -1,21 +1,39 @@
+import 'package:borsellino/theme/sizes.dart';
 import 'package:flutter/material.dart';
 
+/// Represents a generic form text input.
 class FormItem extends StatelessWidget {
-  final fieldDescription;
-  final fieldLabel;
-  TextInputType keyboardType;
+  final String fieldDescription;
+  final String fieldLabel;
+  final TextInputType keyboardType;
+  final ValueChanged<String> onChanged;
 
-  FormItem({this.fieldDescription, this.fieldLabel, this.keyboardType});
+  FormItem({
+    @required this.fieldDescription,
+    @required this.fieldLabel,
+    @required this.keyboardType,
+    @required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final titleTextStyle = TextStyle(
+      fontSize: FontSize.MEDIUM,
+      fontWeight: FontWeight.bold,
+      color: Theme.of(context).primaryColorDark,
+    );
+
     return Container(
       padding: EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: <Widget>[
-          Text(fieldDescription, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+          Text(fieldDescription, style: titleTextStyle),
           TextFormField(
-            decoration: InputDecoration(labelText: fieldLabel, alignLabelWithHint: true),
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              labelText: fieldLabel,
+              alignLabelWithHint: true,
+            ),
             keyboardType: keyboardType,
             validator: (value) {
               if (value.isEmpty) {
--- lib/pages/send_coins/components/send_coins_form.dart
+++ lib/pages/send_coins/components/send_coins_form.dart
@@ -10,41 +10,90 @@ class SendCoinsForm extends StatefulWidget {
   _SendCoinsFormState createState() => _SendCoinsFormState();
 }
 
-class _SendCoinsFormState extends State<SendCoinsForm> {
-  final _formKey = GlobalKey<FormState>();
+enum FormStatus { VALID, INVALID, UNKNOWN }
 
-  int _radioValue = -1;
+class _SendCoinsFormState extends State<SendCoinsForm> {
+  FormStatus _formStatus = FormStatus.UNKNOWN;
+
+  // Fees to be displayed
+  final _initialFeeIndex = 1;
+  final _fees = [0.000001, 0.000250, 0.002500];
+
+  // User input data
+  String _recipient = "";
+  String _amount = "";
+  double _fee = 0.0;
+
+  @override
+  void initState() {
+    _fee = _fees[_initialFeeIndex];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(20),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(3),
-      ),
+      padding: EdgeInsets.all(16),
       child: Form(
         child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               FormItem(
                 fieldDescription: 'Recipient',
                 fieldLabel: 'Recipient Address',
                 keyboardType: TextInputType.text,
+                onChanged: (value) {
+                  setState(() {
+                    _recipient = value;
+                  });
+                },
               ),
+              SizedBox(height: 16),
               FormItem(
                 fieldDescription: 'Amount',
                 fieldLabel: 'Coins amount',
                 keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  _amount = value;
+                },
               ),
-              FormFeeItem(_radioValue),
-              FormSubmitItem(_formKey)
+              SizedBox(height: 16),
+              FormFeeItem(
+                fees: _fees,
+                initialPosition: _initialFeeIndex,
+                onFeeSelected: (value) {
+                  setState(() {
+                    _fee = value;
+                  });
+                },
+              ),
+              SizedBox(height: 32),
+              FormSubmitItem(
+                formState: _formStatus,
+                onPressed: _validateForm,
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _validateForm() {
+    bool isValid = true;
+
+    isValid &= _recipient.trim().isNotEmpty;
+
+    // Check amount
+    isValid &= _amount.trim().isNotEmpty && double.parse(_amount) > 0.0;
+
+    // Check fee
+    isValid &= _fee > 0.0;
+
+    print(isValid);
+
+    setState(() {
+      _formStatus = isValid ? FormStatus.VALID : FormStatus.INVALID;
+    });
   }
 }
